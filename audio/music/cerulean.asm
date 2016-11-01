547c5b576b6410cfe91758db73e493b46d98557e
froggestspirit <froggestspirit@gmail.com>
2016-11-01 16:36:47 -0400

    Music files

---
+++ audio/music/cerulean.asm
@@ -0,0 +1,902 @@
+Music_CeruleanGSC: ; f5127
+	musicheader 4, 1, Music_CeruleanGSC_Ch1
+	musicheader 1, 2, Music_CeruleanGSC_Ch2
+	musicheader 1, 3, Music_CeruleanGSC_Ch3
+	musicheader 1, 4, Music_CeruleanGSC_Ch4
+
+Music_CeruleanGSC_Ch1:
+	volume $77
+	stereopanning $77
+	tempo $94
+	tone $0001
+	dutycycle 2
+	notetype $C, $B2
+	octave 4
+	note E_, 2
+	note D#, 2
+	note C#, 2
+	octave 3
+	note B_, 2
+	note A_, 2
+	note B_, 2
+	octave 4
+	note C#, 2
+	note D#, 2
+Music_CeruleanGSC_Ch1_loop:
+	intensity $B1
+	octave 3
+	note G#, 2
+	octave 4
+	note G#, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note G#, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note G#, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note G#, 2
+	octave 3
+	note A_, 2
+	octave 4
+	note E_, 2
+	octave 3
+	note A_, 2
+	octave 4
+	note E_, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D#, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note D#, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note G#, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note G#, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note G#, 2
+	octave 3
+	note B_, 2
+	octave 4
+	note G#, 2
+	octave 3
+	note A_, 2
+	octave 4
+	note E_, 2
+	octave 3
+	note A_, 2
+	octave 4
+	note E_, 2
+	note D#, 2
+	note B_, 2
+	note D#, 2
+	note B_, 2
+	octave 3
+	note B_, 4
+	note B_, 2
+	note G#, 1
+	note G#, 1
+	note E_, 2
+	note F#, 2
+	intensity $B4
+	note G#, 1
+	note A_, 1
+	note B_, 1
+	octave 4
+	note C#, 1
+	octave 3
+	note B_, 6
+	octave 4
+	note C#, 1
+	octave 3
+	note B_, 1
+	note A_, 8
+	intensity $B2
+	note G#, 1
+	note G#, 1
+	note G#, 1
+	note G#, 1
+	note G#, 2
+	note G#, 1
+	note G#, 1
+	note E_, 2
+	note F#, 2
+	intensity $B4
+	note G#, 1
+	note A_, 1
+	note B_, 1
+	octave 4
+	note C#, 1
+	octave 3
+	note B_, 6
+	note G#, 1
+	note B_, 1
+	octave 4
+	note D#, 8
+	octave 3
+	note E_, 2
+	note E_, 1
+	note F#, 1
+	note G#, 1
+	note __, 3
+	note B_, 4
+	note G#, 4
+	note B_, 2
+	note B_, 1
+	octave 4
+	note C#, 1
+	note D#, 1
+	note __, 3
+	note D#, 4
+	octave 3
+	note B_, 4
+	note A_, 1
+	note G#, 1
+	note A_, 1
+	note B_, 1
+	octave 4
+	note C#, 1
+	note __, 1
+	note C#, 1
+	octave 3
+	note B_, 1
+	octave 4
+	note C#, 1
+	note D#, 1
+	note E_, 1
+	note __, 1
+	note E_, 1
+	note F#, 1
+	note E_, 1
+	note F#, 1
+	octave 5
+	note B_, 1
+	note F#, 1
+	note D#, 1
+	note C#, 1
+	octave 4
+	note B_, 1
+	octave 5
+	note C#, 1
+	note D#, 1
+	note F#, 1
+	note B_, 8
+	intensity $B6
+	octave 3
+	note G#, 8
+	note G#, 4
+	note B_, 4
+	octave 4
+	note E_, 4
+	note F#, 4
+	note G#, 8
+	note D#, 8
+	octave 3
+	note B_, 4
+	octave 4
+	note B_, 4
+	note A_, 4
+	note B_, 4
+	intensity $B2
+	note E_, 2
+	note D#, 2
+	note C#, 2
+	note C_, 2
+	loopchannel 0, Music_CeruleanGSC_Ch1_loop
+
+Music_CeruleanGSC_Ch2:
+	vibrato $12, $34
+	dutycycle 2
+	tone $0002
+	notetype $4, $C2
+	note __, 1
+	octave 5
+	note E_, 6
+	note D#, 6
+	note C#, 6
+	octave 4
+	note B_, 6
+	note A_, 6
+	note B_, 6
+	octave 5
+	note C#, 6
+	note D#, 5
+Music_CeruleanGSC_Ch2_loop:
+	notetype $C, $C2
+	note E_, 6
+	octave 3
+	note E_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C#, 2
+	note D#, 2
+	note E_, 1
+	note F#, 1
+	note G#, 1
+	note A_, 1
+	intensity $C4
+	note G#, 6
+	note A_, 1
+	note G#, 1
+	note F#, 14
+	intensity $C2
+	note E_, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C#, 2
+	note D#, 2
+	note E_, 1
+	note F#, 1
+	note G#, 1
+	note A_, 1
+	intensity $C4
+	note G#, 6
+	note E_, 1
+	note G#, 1
+	note B_, 14
+	intensity $C2
+	dutycycle 1
+	octave 4
+	note E_, 1
+	octave 3
+	note B_, 1
+	octave 4
+	note C#, 2
+	note D#, 2
+	note E_, 1
+	note F#, 1
+	note G#, 1
+	note A_, 1
+	intensity $C4
+	note G#, 6
+	note A_, 1
+	note G#, 1
+	note F#, 8
+	intensity $C2
+	note B_, 1
+	note B_, 1
+	note B_, 1
+	note B_, 1
+	note B_, 2
+	note E_, 1
+	octave 3
+	note B_, 1
+	octave 4
+	note C#, 2
+	note D#, 2
+	note E_, 1
+	note F#, 1
+	note G#, 1
+	note A_, 1
+	intensity $C4
+	note G#, 6
+	note E_, 1
+	note G#, 1
+	note B_, 8
+	octave 3
+	note C#, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C#, 1
+	note D#, 1
+	note E_, 1
+	note __, 3
+	dutycycle 0
+	octave 4
+	note G#, 4
+	note E_, 4
+	dutycycle 1
+	octave 3
+	note D#, 1
+	note C#, 1
+	note D#, 1
+	note E_, 1
+	note F#, 1
+	note __, 3
+	dutycycle 0
+	octave 4
+	note B_, 4
+	note F#, 4
+	dutycycle 1
+	octave 3
+	note C#, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C#, 1
+	note D#, 1
+	note E_, 1
+	note __, 1
+	note E_, 1
+	note D#, 1
+	note E_, 1
+	note F#, 1
+	note G#, 1
+	note __, 1
+	note G#, 1
+	note A_, 1
+	note G#, 1
+	note A_, 1
+	note B_, 1
+	note F#, 1
+	note D#, 1
+	note C#, 1
+	octave 2
+	note B_, 1
+	octave 3
+	note C#, 1
+	note D#, 1
+	note F#, 1
+	note B_, 8
+	intensity $C6
+	dutycycle 2
+	octave 4
+	note E_, 8
+	octave 3
+	note B_, 4
+	octave 4
+	note F#, 4
+	note G#, 4
+	note A_, 4
+	note B_, 8
+	note B_, 8
+	note G#, 4
+	octave 5
+	note D#, 4
+	note C#, 4
+	note D#, 4
+	notetype $4, $C2
+	note __, 1
+	note E_, 6
+	note D#, 6
+	note C#, 6
+	note D#, 5
+	loopchannel 0, Music_CeruleanGSC_Ch2_loop
+
+Music_CeruleanGSC_Ch3:
+	notetype 6, $15
+	vibrato 12, $24
+;Channel3_Bar1:
+	note __, 16
+	octave 4
+	note E_, 3
+	note __, 1
+	note D#, 3
+	note __, 1
+	note C#, 3
+	note __, 1
+	note C_, 3
+	note __, 1
+;Channel3_Bar2:
+Music_CeruleanGSC_Loop3:
+	octave 1
+	note E_, 2
+	note __, 6
+	note E_, 2
+	note __, 4
+	note E_, 1
+	note __, 1
+	note E_, 2
+	note __, 2
+	octave 2
+	note E_, 2
+	note __, 2
+	octave 1
+	note E_, 2
+	note __, 2
+	octave 2
+	note E_, 2
+	note __, 2
+	octave 1
+	note F#, 2
+	note __, 6
+	note F#, 2
+	note __, 4
+	octave 2
+	note C#, 2
+	octave 1
+	note B_, 2
+	note __, 2
+	octave 2
+	note B_, 2
+	note __, 2
+	octave 1
+	note B_, 2
+	note __, 2
+	octave 2
+	note B_, 2
+	note __, 2
+	octave 1
+	note E_, 2
+	note __, 6
+	note E_, 2
+	note __, 4
+	note E_, 1
+	note __, 1
+	note E_, 2
+	note __, 2
+	octave 2
+	note E_, 2
+	note __, 2
+	octave 1
+	note E_, 2
+	note __, 2
+	octave 2
+	note E_, 2
+	note __, 2
+	octave 1
+	note F#, 2
+	note __, 6
+	note F#, 2
+	note __, 4
+	octave 2
+	note C#, 2
+	octave 1
+	note B_, 2
+	note __, 2
+	octave 2
+	note B_, 2
+	note __, 2
+	octave 1
+	note B_, 2
+	octave 2
+	note B_, 2
+	octave 4
+	note B_, 2
+	octave 3
+	note B_, 2
+	octave 1
+	note E_, 2
+	note __, 2
+	octave 5
+	note G#, 1
+	note __, 3
+	octave 1
+	note E_, 2
+	note __, 2
+	octave 5
+	note G#, 1
+	note __, 1
+	octave 1
+	note E_, 1
+	note __, 1
+	note E_, 2
+	note __, 2
+	octave 5
+	note G#, 1
+	note __, 1
+	octave 2
+	note E_, 2
+	octave 1
+	note E_, 2
+	octave 2
+	note E_, 2
+	octave 5
+	note G#, 1
+	note __, 1
+	octave 1
+	note E_, 2
+	note F#, 2
+	note __, 2
+	octave 6
+	note C#, 1
+	note __, 3
+	octave 1
+	note F#, 2
+	note __, 2
+	octave 6
+	note C#, 1
+	note __, 1
+	octave 2
+	note C#, 2
+	octave 1
+	note B_, 2
+	note __, 2
+	octave 6
+	note D#, 1
+	note __, 1
+	octave 2
+	note B_, 2
+	octave 1
+	note B_, 2
+	octave 2
+	note B_, 2
+	octave 6
+	note D#, 1
+	note __, 1
+	octave 1
+	note B_, 2
+	octave 2
+	note E_, 2
+	octave 3
+	note E_, 2
+	octave 4
+	note E_, 2
+	octave 5
+	note E_, 2
+	octave 1
+	note E_, 2
+	note __, 2
+	octave 5
+	note G#, 1
+	note __, 1
+	octave 1
+	note E_, 1
+	note __, 1
+	note E_, 2
+	note __, 2
+	octave 5
+	note G#, 1
+	note __, 1
+	octave 2
+	note E_, 2
+	octave 1
+	note E_, 2
+	octave 2
+	note E_, 2
+	octave 5
+	note G#, 1
+	note __, 1
+	octave 1
+	note E_, 2
+	note F#, 2
+	note __, 2
+	octave 5
+	note E_, 1
+	note __, 3
+	octave 1
+	note F#, 2
+	note __, 2
+	octave 5
+	note E_, 1
+	note __, 1
+	octave 2
+	note C#, 2
+	octave 1
+	note B_, 2
+	note __, 2
+	octave 5
+	note F#, 1
+	note __, 1
+	octave 1
+	note B_, 2
+	note G#, 2
+	octave 4
+	note D#, 2
+	octave 5
+	note G#, 1
+	note __, 1
+	octave 3
+	note G#, 2
+	octave 2
+	note C#, 2
+	note __, 6
+	note C#, 2
+	note __, 4
+	note C#, 1
+	note __, 1
+	note C#, 2
+	note __, 2
+	octave 3
+	note C#, 2
+	note __, 2
+	octave 2
+	note C#, 2
+	note __, 2
+	octave 3
+	note C#, 2
+	note __, 2
+	octave 1
+	note G#, 2
+	note __, 6
+	note G#, 2
+	note __, 4
+	note G#, 1
+	note __, 1
+	note G#, 2
+	note __, 2
+	octave 2
+	note G#, 2
+	note __, 2
+	octave 1
+	note G#, 2
+	note __, 2
+	octave 2
+	note G#, 2
+	note __, 2
+	octave 1
+	note A_, 4
+	octave 2
+	note A_, 2
+	note __, 2
+	octave 1
+	note A_, 2
+	note __, 2
+	note G#, 4
+	octave 2
+	note G#, 2
+	note __, 2
+	octave 1
+	note G#, 2
+	note __, 2
+	note F#, 4
+	octave 2
+	note F#, 2
+	note __, 2
+	octave 1
+	note F#, 2
+	note __, 2
+	note B_, 2
+	note __, 2
+	note B_, 8
+	note __, 16
+	note __, 4
+	octave 2
+	note E_, 2
+	note __, 2
+	octave 1
+	note E_, 2
+	note __, 2
+	octave 2
+	note E_, 2
+	note __, 6
+	note E_, 2
+	note __, 2
+	octave 1
+	note E_, 2
+	note __, 2
+	octave 2
+	note E_, 2
+	note __, 6
+	note F#, 2
+	note __, 2
+	octave 1
+	note F#, 2
+	note __, 2
+	octave 2
+	note F#, 2
+	note __, 6
+	note F#, 2
+	note __, 2
+	octave 1
+	note F#, 2
+	note __, 2
+	octave 2
+	note F#, 2
+	note __, 6
+	note G#, 2
+	note __, 2
+	octave 1
+	note G#, 2
+	note __, 2
+	octave 2
+	note G#, 2
+	note __, 6
+	note G#, 2
+	note __, 2
+	octave 1
+	note G#, 2
+	note __, 2
+	octave 2
+	note G#, 2
+	note __, 6
+	note A_, 2
+	note __, 2
+	octave 1
+	note A_, 2
+	note __, 2
+	octave 2
+	note A_, 2
+	note __, 6
+	note A_, 2
+	note __, 2
+	octave 1
+	note A_, 2
+	note __, 2
+	octave 2
+	note A_, 2
+	note __, 2
+	loopchannel 0, Music_CeruleanGSC_Loop3
+
+Music_CeruleanGSC_Ch4:
+	notetype $C
+	togglenoise 0
+	note __, 16
+Music_CeruleanGSC_Ch4_loop:
+	note A#, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note F_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note A#, 2
+	note F_, 1
+	note F_, 1
+	note G_, 1
+	note F_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note A#, 2
+	note D_, 2
+	note D_, 1
+	note F_, 1
+	note A#, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note A#, 2
+	note A#, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note A_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note D_, 2
+	note D_, 12
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note D_, 2
+	note G_, 1
+	note G_, 1
+	note A#, 2
+	note D_, 2
+	note F_, 1
+	note F_, 1
+	loopchannel 0, Music_CeruleanGSC_Ch4_loop
